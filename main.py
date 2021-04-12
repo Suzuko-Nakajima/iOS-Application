5e928e73c5187a5007d904270ee2f1b0b19b79f2
Nakajima, Suzuko <>
2021-04-12 03:49:18 +0000

    Update v2021.4.12.1

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.accordion import Accordion, AccordionItem
+from kivy.uix.slider import Slider
+from kivy.uix.widget import Widget
+from kivy.uix.image import Image
+
+
+class Form(ButtonBehavior, Image):
+    def __init__(self, **kwargs):
+        super(Form, self).__init__(**kwargs)
+        self.source = 'assets/images/Lucina.jpg'
+
+    def on_press(self):
+        self.source = 'assets/images/Roxy.jpeg'
+
+    def on_release(self):
+        self.source = 'assets/images/Lucina.jpg'
 
 class Naka(App):
     def build(self):
-        return Label(text = "Class Rank: 67/116")
+
+        return Form()
+
 
 
 if __name__ == "__main__":
